3dc5d9bf884b5d06cb2359c538962c0396419752
Prachi-Sharma-17 <149515889+Prachi-Sharma-17@users.noreply.github.com>
2023-12-08 16:00:31 +0530

    Add files via upload

---
+++ simon.js
@@ -0,0 +1,63 @@
+let gameSeq=[];
+let userSeq=[];
+let btns=["red","yellow","green","purple"];
+let started=false;
+let level=0;
+let h2=document.querySelector("h2");
+document.addEventListener("keypress", function(){
+    if(started==false) {
+      console.log("game is started");
+      started=true;
+      levelUp();
+    }
+});
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor); 
+    console.log(gameSeq);
+    gameFlash(randBtn);
+} 
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 200); // time for flash
+}
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    }, 200); // time for flash
+}
+function btnpress(){
+    let btn=this;
+    userFlash(btn);
+    userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+let allbtns=document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click",btnpress);
+}
+function checkAns(idx){
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,300); // 300 is the time interval in which the btn will flash
+        }
+    }else{
+         h2.innerHTML=`Game over! Your score was <b>${level}</b> <br>Press any key to start`;
+        reset();
+    }
+}
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
